e87dbf86c616aa3e57b999e7b6213abd7512f3a0
SivaSuryaPrasad <prasad.chanti21@gmail.com>
2020-04-20 19:29:23 +0530

    Update ChangeDrive.ps1

--- ChangeDrive.ps1
+++ ChangeDrive.ps1
@@ -1,14 +1,13 @@
 #Drive Letter Changing
-Install-WindowsFeature -name Web-Server -IncludeManagementTools
 
-#Get-WmiObject -Class Win32_volume -Filter 'DriveType=5' | Select-Object -First 1 | Set-WmiInstance -Arguments @{DriveLetter='U:'}
+Get-WmiObject -Class Win32_volume -Filter 'DriveType=5' | Select-Object -First 1 | Set-WmiInstance -Arguments @{DriveLetter='U:'}
 $DriveLetters = (Get-WmiObject -Class Win32_volume -Filter 'DriveType=3').DriveLetter
-$Newdriveletter= "G"
+
 foreach($DriveLetter in $DriveLetters)
 {
-    if($DriveLetter -eq "F:")
+    if($DriveLetter -eq "E:")
 	{
-        Get-Partition -DriveLetter $DriveLetter.Trim(":") | Set-Partition -NewDriveLetter $Newdriveletter
+        Get-Partition -DriveLetter $DriveLetter.Trim(":") | Set-Partition -NewDriveLetter F
     }
 }
 
